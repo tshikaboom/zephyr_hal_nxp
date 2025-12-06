8b9d100423fe4152252006b42f7b326d267df408
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2025-12-07 01:46:40 +0200

    Revert "sdk_ng: usb: Simplify some attribute defines to only declare the section"
    This reverts commit 5f23c7e4c8b45c24228eb86c2349a3f4ad564d0c.

--- mcux/mcux-sdk-ng/middleware/usb/include/usb_misc.h
+++ mcux/mcux-sdk-ng/middleware/usb/include/usb_misc.h
@@ -373,12 +373,12 @@ _Pragma("diag_suppress=Pm120")
 #define USB_WEAK_FUN                 __attribute__((weak))
 #define USB_RAM_ADDRESS_ALIGNMENT(n) __attribute__((aligned(n)))
 #define USB_LINK_DMA_INIT_DATA(sec)  __attribute__((section(#sec)))
-#define USB_LINK_USB_GLOBAL          __attribute__((section("m_usb_global")))
-#define USB_LINK_USB_BDT             __attribute__((section("m_usb_bdt")))
+#define USB_LINK_USB_GLOBAL          __attribute__((section("m_usb_global, \"aw\", %nobits " CS)))
+#define USB_LINK_USB_BDT             __attribute__((section("m_usb_bdt, \"aw\", %nobits " CS)))
 #define USB_LINK_USB_GLOBAL_BSS
 #define USB_LINK_USB_BDT_BSS
-#define USB_LINK_DMA_NONINIT_DATA      __attribute__((section("CacheLineData")))
-#define USB_LINK_NONCACHE_NONINIT_DATA __attribute__((section("NonCacheable")))
+#define USB_LINK_DMA_NONINIT_DATA      __attribute__((section("CacheLineData, \"aw\", %nobits " CS)))
+#define USB_LINK_NONCACHE_NONINIT_DATA __attribute__((section("NonCacheable, \"aw\", %nobits " CS)))
 
 #elif (defined(__DSC__) && defined(__CW__))
 #define MAX(a, b)                    (((a) > (b)) ? (a) : (b))
